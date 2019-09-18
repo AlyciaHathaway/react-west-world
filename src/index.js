fcb415b4811577e6c600c769575374eba4a5d38d
Dolores <alyciahathaways@gmail.com>
2019-09-18 09:42:56 +0800

    原生 JS 实现 JSX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+const div = (
+    createElement('div',
+        createElement('p',
+            createElement('span', 'hi')))
+)
+// 如果发明一种语法，用户写 div2，我翻译成 div
+// const div2 = (
+//     <div>
+//         <p>
+//             <span>hi</span>
+//         </p>
+//     </div>
+// )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+document.body.appendChild(div)
+
+function createElement(tagName, children) {
+    const element = document.createElement(tagName)
+    if (children) {
+        if (typeof children === 'string') {
+            let childrenElement = document.createTextNode(children)
+            element.appendChild(childrenElement)
+        } else {
+            element.appendChild(children)
+        }
+    }
+    return element
+}
