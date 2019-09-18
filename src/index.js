5f1f34a5f80cc732f0c3bd7bc1789e4a90c536d2
Dolores <alyciahathaways@gmail.com>
2019-09-18 19:02:16 +0800

    更新棋盘的渲染逻辑

--- src/index.js
+++ src/index.js
@@ -4,22 +4,36 @@ import './style.css'
 
 const Cell = function (props) {
     return (
-        <div className="cell">{props.text}</div>
+        <div className="cell" onClick={props.onClick}>{props.text}</div>
     )
 }
 
-const cells = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
-
 const Chessboard = function () {
+    const [cells, setCells] = React.useState([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ])
+    const [n, setN] = React.useState(0)
+    const tell = () => {
+
+    }
+    const onClickCell = (row, col) => {
+        // 点击次数
+        setN(n + 1)
+        // 改变 cells
+        const copy = JSON.parse(JSON.stringify(cells))
+        copy[row][col] = n % 2 === 0 ? 'x' : 'o'
+        setCells(copy)
+        // 判断谁赢
+        tell()
+    }
     return (
         <div>
-            {cells.map(items => <div className="row">
-                {items.map(item => <div className="col">
-                    <Cell text={item} />
+            <div>第{n}次点击</div>
+            {cells.map((items, row) => <div className="row">
+                {items.map((item, col) => <div className="col">
+                    <Cell text={item} onClick={() => onClickCell(row, col)} />
                 </div>)}
             </div>)}
         </div>
