9528b638effc33f358dce6ff0fe577c90a48307d
Dolores <alyciahathaways@gmail.com>
2019-11-19 22:26:53 +0800

    函数组件与 class 组件

---
+++ src/add.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Add(props) {
+    return (
+        <div>
+            <Box name="新垣结衣即是正义" />
+        </div>
+    )
+}
+
+class Box extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 0
+        }
+    }
+    add() {
+        // 正确
+        this.setState(state => {
+            return { number: state.number + 1 }
+        })
+        this.setState(state => {
+            return { number: state.number + 1 }
+        })
+    }
+    minus() {
+        // 错误
+        this.setState({
+            number: this.state.number - 1
+        })
+        this.setState({
+            number: this.state.number - 1
+        })
+    }
+    render() {
+        return (
+            <div className="red">
+                <span>{this.state.number}</span>
+                <button onClick={this.add.bind(this)}>+2</button>
+                <button onClick={this.minus.bind(this)}>-2</button>
+                {this.props.name}
+            </div>
+        )
+    }
+}
+
+export { Add }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Chessboard } from './chessboard'
+import { Add } from './add'
 
-
-
-ReactDOM.render(<div>
-    <Chessboard />
-</div>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Chessboard />
+        <Add />
+    </div>,
+    document.getElementById('root')
+)
